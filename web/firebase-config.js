921f24ea54274d45c7d20c100519147a3c68fdd2
Robin T <69640665+Lerobindesbois@users.noreply.github.com>
2022-05-26 17:04:30 -0400

    Delete web directory

--- web/firebase-config.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDNcdwvj9NhTZ4nIKRrZpgLRYuwZqQI81w",
-    authDomain: "selectic-iot.firebaseapp.com",
-    databaseURL: "https://selectic-iot.firebaseio.com",
-    projectId: "selectic-iot",
-    storageBucket: "selectic-iot.appspot.com",
-    messagingSenderId: "411055761702",
-    appId: "1:411055761702:web:36b671d964feb02c1e79e7"
-};
